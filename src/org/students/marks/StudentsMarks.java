d6f2fce6fd0a4d67eb0251f6e5ef1a9aa6d0c9b9
Karthika-0423 <Karthikakith@gmail.com>
2025-04-02 23:49:05 +0530

    Marks Added

---
+++ src/org/students/marks/StudentsMarks.java
@@ -0,0 +1,33 @@
+package org.students.marks;
+
+import java.util.Scanner;
+
+public class StudentsMarks {
+	
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	   
+	System.out.println("Students ID ");   
+	Short id =sc.nextShort();
+	System.out.println("Students ID :"+id);
+	
+	System.out.println("Students Name ");   
+	String name =sc.next();
+	System.out.println("Students Name :"+name);
+	
+	System.out.println("Mark 1: ");   
+	Short mark1 = sc.nextShort();
+	System.out.println("Students Mark1 :"+mark1);
+		
+	System.out.println("Mark 2: ");   
+	Short mark2 =sc.nextShort();
+	System.out.println("Students Mark2 : "+mark2);
+	
+	System.out.println("Mark 3: ");   
+	Short mark3 = sc.nextShort();
+	System.out.println("Students Mark3 : "+mark3);
+	
+	}
+}
